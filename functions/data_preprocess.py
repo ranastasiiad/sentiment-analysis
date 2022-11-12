f24e4c7464788d14fcaa8dfb63384f9ab35e109d
anastasiia <rozhkovaad@my.msu.ru>
2022-11-12 19:53:52 +0300

    working pipeline with w2v

---
+++ functions/data_preprocess.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+from tqdm.notebook import tqdm
+import re
+import string
+
+punct = set(string.punctuation)
+
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download("stopwords")
+stop_words = set(stopwords.words("english"))
+# print(stop_words)
+from nltk.stem.porter import *
+
+stemmer = PorterStemmer()
+
+from bs4 import BeautifulSoup
+
+
+def text_cleaning(df, review_column):
+    df['tidy_review'] = df[f'{review_column}']
+
+    for j in tqdm(range(df.shape[0])):
+        # берем один отзыв
+        text = df[f'{review_column}'][j]
+        # все слова к одному регистру
+        text = text.lower()
+        # html
+        text = BeautifulSoup(text, 'lxml').text
+        # ненужные символы и знаки
+        text = re.sub('[^A-Za-z0-9]+', ' ', text)
+        text = re.sub('[+\-*/]', ' ', text)
+        text = re.sub('\d+(\.\d*)?', ' ', text)
+        # убираем стоп-слова
+        text = " ".join([word for word in text.split() if word not in stop_words])
+        # пунктуация
+        text = "".join([p for p in text if p not in punct])
+        # достаем токены
+        text = text.split()
+        # стемминг
+        text = [stemmer.stem(i) for i in text]
+        # соединяем обратно
+        text = ' '.join(text)
+
+        df['tidy_review'][j] = text
+
+    return df
---
+++ functions/models_evaluation.py
@@ -0,0 +1,37 @@
+from copy import deepcopy
+
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import KFold
+
+
+## Функция для расчета метрики модели на кросс-валидации
+def cv_score(
+        X,  # dataframe
+        y,  # target vector
+        model,  # regressor, classifier or pipeline
+        metric=roc_auc_score,  # metric from competition
+        n_splits=5,
+        shuffle=False,
+        random_state=42):
+
+    kf = KFold(n_splits=n_splits, shuffle=shuffle, random_state=random_state)
+    metrics = list()
+
+    # Кросс-валидация
+    for i, (train_index, valid_index) in enumerate(kf.split(X, y)):
+        X_train = X.loc[train_index]
+        y_train = y.loc[train_index].values
+
+        X_valid = X.loc[valid_index]
+        y_valid = y.loc[valid_index].values
+
+        model_kf = deepcopy(model)
+
+        model_kf.fit(X_train, y_train)
+
+        prediction = model_kf.predict_proba(X_valid)[:,1]
+        cur_metric = metric(y_valid, prediction)
+
+        metrics.append(cur_metric)
+
+    return metrics
