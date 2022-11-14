efb396c03756e7c0120c08dc01737327ddc47845
anastasiia <rozhkovaad@my.msu.ru>
2022-11-14 23:21:20 +0300

    light_cleaning and half of transformer

--- functions/data_preprocess.py
+++ functions/data_preprocess.py
@@ -8,13 +8,15 @@ punct = set(string.punctuation)
 
 import nltk
 from nltk.corpus import stopwords
-
 nltk.download("stopwords")
 stop_words = set(stopwords.words("english"))
 # print(stop_words)
-from nltk.stem.porter import *
 
+from nltk.stem.porter import *
 stemmer = PorterStemmer()
+
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
 
 from bs4 import BeautifulSoup
 
@@ -30,9 +32,8 @@ def text_cleaning(df, review_column):
         # html
         text = BeautifulSoup(text, 'lxml').text
         # ненужные символы и знаки
-        text = re.sub('[^A-Za-z0-9]+', ' ', text)
-        text = re.sub('[+\-*/]', ' ', text)
-        text = re.sub('\d+(\.\d*)?', ' ', text)
+        text = re.sub('[+\-*/]', '', text)
+        text = re.sub('\d+(\.\d*)?', '', text)
         # убираем стоп-слова
         text = " ".join([word for word in text.split() if word not in stop_words])
         # пунктуация
@@ -41,9 +42,29 @@ def text_cleaning(df, review_column):
         text = text.split()
         # стемминг
         text = [stemmer.stem(i) for i in text]
+        # лемминг
+        # text = [lemmatizer.lemmatize(i) for i in text]
         # соединяем обратно
         text = ' '.join(text)
 
+        df['tidy_review'][j] = text
+
+    return df
+
+
+def light_text_cleaning(df, review_column):
+    df['tidy_review'] = df[f'{review_column}']
+
+    for j in tqdm(range(df.shape[0])):
+        # берем один отзыв
+        text = df[f'{review_column}'][j]
+        # все слова к одному регистру
+        # text = text.lower()
+        # html
+        text = BeautifulSoup(text, 'lxml').text
+        # ненужные символы и знаки
+        text = re.sub('[+\-*/]', '', text)
+        text = re.sub('\d+(\.\d*)?', '', text)
         df['tidy_review'][j] = text
 
     return df
